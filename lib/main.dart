ccf60699f1dbf95670f1d67ff0b0ca3777d3c81d
Yauheni Stanulevich <144230979+stanulpych@users.noreply.github.com>
2024-10-04 09:54:55 +0300

    added ability to use firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -9,11 +10,20 @@ import 'package:talker_dio_logger/talker_dio_logger_interceptor.dart';
 import 'package:talker_dio_logger/talker_dio_logger_settings.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 import 'crypto_coins_list_app.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+
   final talker = TalkerFlutter.init();
   GetIt.I.registerSingleton(talker);
   GetIt.I<Talker>().debug('Talker started...');
+
+  // WidgetsFlutterBinding.ensureInitialized();
+  // final app = await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  //
+  // talker.info(app.options.projectId);
 
   final dio = Dio();
   dio.interceptors.add(
@@ -39,7 +49,6 @@ void main() {
 
   FlutterError.onError =
       (details) => GetIt.I<Talker>().handle(details.exception, details.stack);
-
   runZonedGuarded(
           () => runApp(const CryptoCurrenciesListApp()),
           (error, stack) => GetIt.I<Talker>().handle(error, stack),
