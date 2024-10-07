a80c846051b7fef9440482ff9bbea5dae66d4ddd
Yauheni Stanulevich <144230979+stanulpych@users.noreply.github.com>
2024-10-07 22:05:19 +0300

    added localization

--- lib/crypto_coins_list_app.dart
+++ lib/crypto_coins_list_app.dart
@@ -2,8 +2,11 @@ import 'package:crypto_coins_list/router/router.dart';
 import 'package:crypto_coins_list/theme/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get_it/get_it.dart';
 import 'package:talker_flutter/talker_flutter.dart';
+
+import 'generated/l10n.dart';
 
 class CryptoCurrenciesListApp extends StatelessWidget {
   CryptoCurrenciesListApp({super.key});
@@ -21,6 +24,13 @@ class CryptoCurrenciesListApp extends StatelessWidget {
           TalkerRouteObserver(GetIt.I<Talker>()),
         ],
       ),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
     );
   }
 }
